7bf2d5464d548623ecee2a98a094725f3d44993c
LeifCastle <leifurcastle@gmail.com>
2023-08-11 11:33:08 -0700

    feat: util file for sending daily emails (just a test for scheduler right now)

---
+++ src/utils/dailyEmailSend.js
@@ -0,0 +1,30 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+async function sendScheduledEmail() {
+  const data = {
+    from: {
+      email: "birthday.buzzx@gmail.com",
+      name: "Birthday Buzz",
+    },
+    to: [
+      {
+        email: "leifurcastle@gmail.com",
+        name: "Mr User",
+      },
+    ],
+    subject: "Birthday Reminder!",
+    text: "It's your birthday!",
+    //send_at: 1617260400,
+  };
+
+  const response = await sgMail.send(data);
+
+  if (response.statusCode === 202) {
+    console.log("Email scheduled successfully!");
+  } else {
+    console.log(response.body);
+  }
+}
+
+sendScheduledEmail();
