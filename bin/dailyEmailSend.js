9c1f2172718639a88cbf665ddedf7368c1574728
LeifCastle <leifurcastle@gmail.com>
2023-08-12 14:19:17 -0700

    feat: basic dailyEmailSend functionality...sends emails only on Day Of at specific time, removed npm fix nonensense from package-lock.json

--- bin/dailyEmailSend.js
+++ bin/dailyEmailSend.js
@@ -1,9 +1,30 @@
 #! /app/bin/node
-
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const axios = require("axios");
+const dotenv = require("dotenv");
 
-async function sendScheduledEmail() {
+dotenv.config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8000";
+
+function getUsers() {
+  const today = new Date().toLocaleDateString().slice(0, -5);
+  console.log("Current Date: ", today);
+  axios.get(`${BASE_URL}/users`).then((response) => {
+    response.data.forEach((user) => {
+      console.log(`In User: ${user.firstName} ${user.lastName}`);
+      user.buzzList.forEach((person) => {
+        if (person.birthday.slice(0, -5) === today) {
+          console.log(`Birthday Boy: ${person.name}`);
+          console.log(person.reminderTimeFrame);
+          sendScheduledEmail(user.email, user.firstName, person.message); //Need to also pass user time of day sending preference as a parameter
+        }
+      });
+    });
+  });
+}
+
+async function sendScheduledEmail(address, name, message) {
   const data = {
     from: {
       email: "birthday.buzzx@gmail.com",
@@ -11,22 +32,21 @@ async function sendScheduledEmail() {
     },
     to: [
       {
-        email: "leifurcastle@gmail.com",
-        name: "Mr User",
+        email: address,
+        name: name,
       },
     ],
-    subject: "Birthday Reminder!",
-    text: "It's your birthday!",
+    subject: "Birthday Reminder!", //Future Feature: Let user customize this
+    text: message,
     //send_at: 1617260400,
   };
 
   const response = await sgMail.send(data);
-
-  if (response.statusCode === 202) {
-    console.log("Email scheduled successfully!");
-  } else {
-    console.log(response.body);
-  }
+  response.forEach((email) => {
+    if (email.statusCode === 202) {
+      console.log("Email scheduled successfully!");
+    }
+  });
 }
 
-sendScheduledEmail();
+getUsers();
