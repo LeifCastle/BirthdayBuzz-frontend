a9cc4fda5eed5b15983aae3671a3f56601b6bc75
LeifCastle <leifurcastle@gmail.com>
2023-08-13 09:14:41 -0700

    finished basic styling and functionality for questionaire

--- src/components/BuzzList/NewEntry.js
+++ src/components/BuzzList/NewEntry.js
@@ -17,17 +17,14 @@ export default function NewEntry({
   const [relation, setRelation] = useState();
   const [message, setMessage] = useState();
   const time = useRef([]);
-  const [delivery, setDelivery] = useState("email");
-  const [publiclySeen, setPubliclySeen] = useState("true");
 
-  //----------Current question values from questionBase
+  //----------Current question values from questionBase.current
   const [question, setQuestion] = useState({
     id: 0,
     inputType: "text",
     title: "What's their name?",
     placeholder: "John",
     value: name,
-    type: "name",
     setValue: setName,
     change: changeText,
   });
@@ -42,14 +39,13 @@ export default function NewEntry({
   const [error, setError] = useState();
 
   //----------Gives questionaire inputs predefined values and temporarily stores user input history
-  let questionBase = [
+  const questionBase = useRef([
     {
       id: 0,
       inputType: "text",
       title: "What's their name?",
       placeholder: "John",
       value: name,
-      type: "name",
       setValue: setName,
       change: changeText,
     },
@@ -59,7 +55,6 @@ export default function NewEntry({
       title: "What's their birthday?",
       placeholder: "01/01/2001",
       value: birthday,
-      type: "birthday",
       setValue: setBirthday,
       change: changeText,
     },
@@ -69,7 +64,6 @@ export default function NewEntry({
       title: "What is this person's relation to you?",
       placeholder: "Friend",
       value: relation,
-      type: "relation",
       setValue: setRelation,
       change: changeText,
     },
@@ -79,59 +73,21 @@ export default function NewEntry({
       title: "What would you like the reminder text to be?",
       placeholder: "It's John's birday today!",
       value: message,
-      type: "message",
       setValue: setMessage,
       change: changeText,
     },
     {
       id: 4,
-      inputType: "checkbox",
-      title: "When do you want to be reminded?",
-      placeholder: "Day Of",
-      name: "Group1",
-      staticValue1: "Day Of",
-      staticValue2: "Day Before",
-      value: time.current,
-      type: "time",
-      setValue: changeTime,
-      change: changeTime,
+      title: "When would you like to be reminded?",
+      one: true,
+      two: false,
     },
     {
       id: 5,
-      inputType: "radio",
-      title: "Would you like an email or text reminder?",
-      placeholder: "text",
-      value: delivery,
-      type: "delivery",
-      name: "Group2",
-      staticValue1: "email",
-      staticValue2: "text",
-      change: changeReminder,
-      setValue: setDelivery,
+      title: "s",
+      value: "Empty",
     },
-    {
-      id: 6,
-      inputType: "radio",
-      title:
-        "Do you want other users to be able to see this person on your Buzz List?",
-      placeholder: "True",
-      value: publiclySeen,
-      type: "publicalySeen",
-      name: "Group3",
-      staticValue1: "True",
-      staticValue2: "False",
-      change: changePublic,
-      setValue: setPubliclySeen,
-    },
-    //Empty to prevent input from trying to grab non-existent 7'th question
-    {
-      id: 7,
-      title: name,
-      placeholder: "",
-      value: "",
-      setValue: "",
-    },
-  ];
+  ]);
 
   const BASE_URL =
     process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8000";
@@ -140,14 +96,14 @@ export default function NewEntry({
 
   //----------Next question
   function handleNextQuestion() {
-    let index = progress.current + 1; //The index of the current question in questionBase
+    let index = progress.current + 1; //The index of the current question in questionBase.current
     if (document.querySelector(".question").value || time.current.length > 0) {
-      document.querySelector(".question").value = "";
-      setQuestion(questionBase[index]);
-      value.current = questionBase[index].value; //Set value ref to last stored value if any
       progress.current += 1;
+      document.querySelector(".question").value = "";
+      value.current = questionBase.current[index].value; //Set value ref to last stored value if any
+      setQuestion(questionBase.current[index]);
     } else {
-      setError(`A ${questionBase[index - 1].type} is required`);
+      setError(`A ${questionBase.current[index - 1].type} is required`);
       setTimeout(() => {
         setError();
       }, 2000);
@@ -158,9 +114,11 @@ export default function NewEntry({
   function handlePreviousQuestion() {
     document.querySelector(".question").value = "";
     let index = progress.current - 1;
-    setQuestion(questionBase[index]);
-    value.current = questionBase[index].value; //Set value ref to last stored value if any
+    setQuestion(questionBase.current[index]);
+    value.current = questionBase.current[index].value; //Set value ref to last stored value if any
+    console.log(questionBase.current[index]);
     progress.current -= 1;
+    console.log("VC: ", questionBase.current[index]);
   }
 
   //----------Cancel entry and return to Buzz List
@@ -172,33 +130,22 @@ export default function NewEntry({
 
   //----------Saves user inputs for type "text/date" in question base
   function changeText(e) {
-    let index = e.target.id;
-    let currentQuestion = questionBase[index];
-    currentQuestion.setValue(e.target.value);
+    let index = parseInt(e.target.id);
+    questionBase.current[index].setValue(e.target.value); //Sets the value in input's setState
+    questionBase.current[index].value = e.target.value; //Sets the value in input's state record
+    value.current = e.target.value; //Actually sets the value on the input equal to what the user types
   }
 
   //----------Saves user inputs from "time" input in question base
   function changeTime(e) {
     if (e.target.checked) {
-      //If the user checks a box
       time.current = [...time.current, e.target.getAttribute("staticvalue")];
     } else {
-      //If the user unchecks a box
       let real = time.current.filter((x) => {
         return x !== e.target.getAttribute("staticvalue");
       });
       time.current = [...real];
     }
-  }
-
-  //----------Saves user inputs from "reminderTimeFrame" input in question base
-  function changeReminder(e) {
-    setDelivery(e.target.getAttribute("staticvalue"));
-  }
-
-  //----------Saves user inputs from "publiclySeen" input in question base
-  function changePublic(e) {
-    setPubliclySeen(e.target.getAttribute("staticvalue"));
   }
 
   //---------------------------------Adding new entry to database---------------------------------\\
@@ -215,8 +162,6 @@ export default function NewEntry({
         relation: relation,
         message: message,
         time: time.current,
-        delivery: delivery,
-        publiclySeen: publiclySeen,
       },
     };
     console.log("New Entry: ", newEntry);
@@ -241,6 +186,7 @@ export default function NewEntry({
 
   //---------------------------------------HTML Visibility----------------------------------------\\
   useEffect(() => {
+    console.log("Progress: ", progress.current);
     //----------If the user is on the first question hide the back button
     if (progress.current === 0) {
       document.querySelector("#backButton").setAttribute("hidden", "hidden");
@@ -248,15 +194,17 @@ export default function NewEntry({
       document.querySelector("#backButton").removeAttribute("hidden");
     }
 
-    //----------If the user is on question four or later show the second input
-    if (progress.current > 3 && progress.current < 7) {
-      document.querySelector("#input2").removeAttribute("hidden");
+    //----------If the user is on question four show the time inputs
+    if (progress.current === 4) {
+      document.querySelector("#questionShell").setAttribute("hidden", "hidden");
+      document.querySelector("#time").removeAttribute("hidden");
     } else {
-      document.querySelector("#input2").setAttribute("hidden", "hidden");
+      document.querySelector("#questionShell").removeAttribute("hidden");
+      document.querySelector("#time").setAttribute("hidden", "hidden");
     }
 
     //----------If the user goes past the last question...
-    if (progress.current >= 7) {
+    if (progress.current > 4) {
       document.querySelector("#create").removeAttribute("hidden");
       document.querySelector("#previewPerson").removeAttribute("hidden");
       document.querySelector("#nextButton").setAttribute("hidden", "hidden");
@@ -270,15 +218,17 @@ export default function NewEntry({
   }, [progress.current]);
 
   return (
-    <div id="flexError" className="flex-col h-[300px] items-center">
+    <div id="flexError" className="flex-col h-[260px] items-center">
       <button
         className="bg-button1 rounded-md w-full h-8 mb-4"
         onClick={cancelEntry}
       >
         Cancel Entry
       </button>
-      <p>{question.title}</p>
-      <div className="flex mt-4 w-full">
+      <p className="h-10 text-center text-lg">
+        {question.title ? question.title : name}
+      </p>
+      <div className="flex w-full">
         <div className="flexError basis-2/5 justify-end">
           <button
             id="backButton"
@@ -288,56 +238,65 @@ export default function NewEntry({
             Back
           </button>
         </div>
-        <div className="w-[200px]" id="previewPerson" hidden="hidden">
-          <ul>
-            <li>
-              <span>Birthday: </span>
-              {birthday}
-            </li>
-            <li>
-              <span>Relation: </span>
-              {relation}
-            </li>
-            <li className="whitespace-nowrap">
-              <span>Message: </span>
-              {message}
-            </li>
-            <li className="whitespace-nowrap">
-              <span>Time: </span>
-              {time.current}
-            </li>
-            <li className="whitespace-nowrap">
-              <span>Delivery: </span>
-              {delivery}
-            </li>
-            <li className="whitespace-nowrap">
-              <span>Publicly Seen: </span>
-              {publiclySeen}
-            </li>
-          </ul>
-        </div>
-        <div className="flexError basis-1/5 justify-center" id="questionaire">
-          <input
-            id={question.id}
-            className="question placeholder:text-slate-400 text-black rounded-md bg-slate-300 pl-2"
-            placeholder={question.placeholder}
-            type={question.inputType}
-            name={question.name}
-            staticvalue={question.staticValue1}
-            value={value.current}
-            onChange={question.change}
-            max="9999-12-31"
-          />
-          <label for="my-radio-1">{question.staticValue1}</label>
-          <input
-            id="input2"
+        <div
+          className="flexError basis-1/5 justify-center items-center"
+          id="questionaire"
+        >
+          <div
+            className="w-[200px] mt-[-12px]"
+            id="previewPerson"
             hidden="hidden"
-            type={question.inputType}
-            name={question.name}
-            staticvalue={question.staticValue2}
-            onChange={question.change}
-          />
-          <label for="my-radio-1">{question.staticValue2}</label>
+          >
+            <ul>
+              <li>
+                <span>Birthday: </span>
+                {birthday}
+              </li>
+              <li>
+                <span>Relation: </span>
+                {relation}
+              </li>
+              <li className="whitespace-nowrap">
+                <span>Message: </span>
+                {message}
+              </li>
+              <li className="whitespace-nowrap">
+                <span>Time: </span>
+                {time.current}
+              </li>
+            </ul>
+          </div>
+          <div id="questionShell">
+            <input
+              id={question.id}
+              className="question placeholder:text-slate-400 text-black rounded-md bg-slate-300 pl-2 h-[40px]"
+              placeholder={question.placeholder}
+              type={question.inputType}
+              name={question.name}
+              staticvalue={question.staticValue1}
+              value={value.current}
+              onChange={question.change}
+              max="9999-12-31"
+            />
+          </div>
+          <div id="time" hidden="hidden">
+            <div className="flex gap-1">
+              <input
+                id="one"
+                type="checkbox"
+                staticvalue="Day Of"
+                onClick={changeTime}
+              />
+              <p className="mr-4">Day Of</p>
+              <input
+                id="two"
+                type="checkbox"
+                onChange={changeTime}
+                staticvalue="Day Before"
+              />
+              <p>Day Before</p>
+            </div>
+          </div>
         </div>
         <div className="flexError basis-2/5 justify-start">
           <button
@@ -354,7 +313,7 @@ export default function NewEntry({
       </div>
       <button
         id="create"
-        className="w-full h-8 bg-button1 rounded-md ml-2 pl-1 pr-1"
+        className="w-full h-14 bg-button1 rounded-md ml-2 pl-1 pr-1"
         hidden="hidden"
         onClick={createEntry}
       >
