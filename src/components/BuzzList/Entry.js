4032db3c71ce39bad5d59492891920d2bcfd78b9
LeifCastle <leifurcastle@gmail.com>
2023-08-12 18:24:41 -0700

    feat: moderate functionality to prevent user from adding a person to their buzzlist with null/unacceptable information, different input types implemented as well

--- src/components/BuzzList/Entry.js
+++ src/components/BuzzList/Entry.js
@@ -2,12 +2,24 @@
 import { useState } from "react";
 
 export default function Entry({ data }) {
-  console.log("Data: ", data);
+  //console.log("Entry: ", data);
   let values = [];
   for (let value in data) {
     if (value != "_id" && value != "createdAt" && value != "updatedAt") {
-      values.push(<p className="basis-1/5 inline-block mr-2">{data[value]}</p>);
+      values.push(
+        <p
+          key={data._id + value}
+          id={data._id + value}
+          className="basis-1/5 inline-block mr-2"
+        >
+          {data[value]}
+        </p>
+      );
     }
   }
-  return <li className="flex ml-2">{values}</li>;
+  return (
+    <li key={data._id} className="flex ml-2">
+      {values}
+    </li>
+  );
 }
