3413a06a6109a7d9c15e457a90a441c3096e0cf1
LeifCastle <leifurcastle@gmail.com>
2023-08-14 02:39:22 -0700

    fix: rerouted user from home page to login page when they aren't signed in

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,6 @@ import PageHeader from "@/components/page_header";
 import BuzzList from "@/components/BuzzList/BuzzList";
 
 export default function Home() {
-<<<<<<< HEAD
   const [HTML, setHTML] = useState();
   const router = useRouter();
   useEffect(() => {
@@ -28,30 +27,4 @@ export default function Home() {
   }, [router]);
 
   return <>{HTML}</>;
-=======
-  const [data, setData] = useState(null);
-  const [isLoading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       // data is an object
-  //       setData("true");
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  // if (isLoading) return <p>Loading...</p>;
-  // if (!data) return <p>No data shown...</p>;
-
-  return (
-    <>
-      <PageHeader />
-      <main id="flexError" className="flex-col items-center pt-4">
-        <BuzzList />
-      </main>
-    </>
-  );
->>>>>>> 6a47d99 (test)
 }
