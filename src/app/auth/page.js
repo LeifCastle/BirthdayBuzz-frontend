4891d98b759f2a9d6abbab8f9f63246a5a13a6d5
LeifCastle <leifurcastle@gmail.com>
2023-08-18 17:14:22 -0700

    FIX: deployment error /auth redirect location undefined

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,10 +1,15 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/auth/login");
+  useEffect(() => {
+    if (window.location) {
+      router.push("/auth/login");
+    }
+  }, [router]);
 
   return;
 }
