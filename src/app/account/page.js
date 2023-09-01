722a3403623bd1350fb041f8639e5685b1ff8cb1
LeifCastle <leifurcastle@gmail.com>
2023-08-31 18:28:06 -0700

    FEAT: added funcionality allowing users to edit their account details, local timezone, and choice of buzz time

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -13,8 +13,32 @@ export default function Account() {
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [birthday, setBirthday] = useState();
+  const [timezone, setTimezone] = useState();
+  const [buzzTime, setBuzzTime] = useState();
   const BASE_URL =
     process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:8000";
+
+  function updateBuzzTime(e) {
+    setBuzzTime(e.target.value);
+    axios
+      .put(`${BASE_URL}/account/buzztime/${localStorage.getItem("email")}`, {
+        buzzTime: e.target.value,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
+  function updateTimezone(e) {
+    setTimezone(e.target.value);
+    axios
+      .put(`${BASE_URL}/account/timezone/${localStorage.getItem("email")}`, {
+        timezone: e.target.value,
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
 
   function handleEditAccount() {
     document.querySelector("#editMyAccount").removeAttribute("hidden");
@@ -54,6 +78,8 @@ export default function Account() {
           setFirstName(response.data[0].firstName);
           setLastName(response.data[0].lastName);
           setBirthday(response.data[0].birthday);
+          setBuzzTime(response.data[0].buzzTime);
+          setTimezone(response.data[0].timezone);
         })
         .catch((error) => {
           console.error(
@@ -70,69 +96,135 @@ export default function Account() {
     return (
       <div className="bg-[url('/static/images/App_Background.png')] w-[100vw] h-[100vh] bg-cover">
         <PageHeader />
-        <div className="flex flexError place-content-between text-slate-200">
-          <div className="bg-slate-100 bg-opacity-[.15] ml-10 rounded-md">
-            <h1 className="text-2xl font-bold text-center bg-cH1 bg-opacity-[1] rounded-tl-md rounded-tr-md">
-              My Account
-            </h1>
-            <hr></hr>
-            <div id="myAccount">
-              <div className="pl-4 pr-4 pt-2 pb-2 w-[300px] h-[130px]">
-                <p>First Name: {userData.firstName}</p>
-                <p className="mt-1">Last Name: {userData.lastName}</p>
-                <p className="mt-1">Email: {userData.email}</p>
-                <p className="mt-1">Birthday: {userData.birthday}</p>
+        <div className="flex-col flexError place-content-between text-slate-200">
+          <div className="flex flexError place-content-between">
+            <div className="bg-slate-100 bg-opacity-[.15] ml-10 rounded-md">
+              <h1 className="text-2xl font-bold text-center bg-cH1 bg-opacity-[1] rounded-tl-md rounded-tr-md">
+                My Account
+              </h1>
+              <hr className="border-slate-400"></hr>
+              <div id="myAccount">
+                <div className="pl-4 pr-4 pt-2 pb-2 w-[300px] h-[130px]">
+                  <p>First Name: {userData.firstName}</p>
+                  <p className="mt-1">Last Name: {userData.lastName}</p>
+                  <p className="mt-1">Email: {userData.email}</p>
+                  <p className="mt-1">Birthday: {userData.birthday}</p>
+                </div>
+                <button
+                  className="bg-button1 rounded-bl-md rounded-br-md mr-2 pl-4 pr-4 h-[40px] w-full hover:bg-opacity-[.8]"
+                  onClick={handleEditAccount}
+                >
+                  Edit
+                </button>
               </div>
-              <button
-                className="bg-button1 rounded-bl-md rounded-br-md mr-2 pl-4 pr-4 h-[40px] w-full hover:bg-opacity-[.8]"
-                onClick={handleEditAccount}
-              >
-                Edit
-              </button>
+              <div id="editMyAccount" hidden="hidden">
+                <div className="pl-4 pr-4 pt-2 pb-2 w-[300px] h-[130px]">
+                  <p>
+                    First Name:
+                    <input
+                      className="ring-slate-200 ring-[1px] rounded-md ml-1 pl-2 w-[150px] bg-transparent bg-opacity-[.5]"
+                      type="text"
+                      value={firstName}
+                      onChange={(e) => setFirstName(e.target.value)}
+                    ></input>
+                  </p>
+                  <p className="mt-1">
+                    Last Name:
+                    <input
+                      className="ring-slate-200 ring-[1px] rounded-md ml-1 pl-2 w-[150px] bg-transparent bg-opacity-[.5]"
+                      type="text"
+                      value={lastName}
+                      onChange={(e) => setLastName(e.target.value)}
+                    ></input>
+                  </p>
+                  <p className="mt-1">Email: {userData.email}</p>
+                  <p className="mt-1">
+                    Birthday:
+                    <input
+                      className="ring-slate-200 ring-[1px] rounded-md ml-1 pl-2 w-[150px] bg-transparent bg-opacity-[.5]"
+                      type="date"
+                      value={birthday}
+                      onChange={(e) => setBirthday(e.target.value)}
+                    ></input>
+                  </p>
+                </div>
+                <button
+                  className="bg-[#404F60] rounded-bl-md rounded-br-md mr-2 pl-4 pr-4 h-[40px] w-full hover:bg-opacity-[.8]"
+                  onClick={saveAccountEdits}
+                >
+                  Save
+                </button>
+              </div>
             </div>
-            <div id="editMyAccount" hidden="hidden">
-              <div className="pl-4 pr-4 pt-2 pb-2 w-[300px] h-[130px]">
-                <p>
-                  First Name:
-                  <input
-                    className="ring-slate-200 ring-[1px] rounded-md ml-1 pl-2 w-[150px] bg-transparent bg-opacity-[.5]"
-                    type="text"
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                  ></input>
-                </p>
-                <p className="mt-1">
-                  Last Name:
-                  <input
-                    className="ring-slate-200 ring-[1px] rounded-md ml-1 pl-2 w-[150px] bg-transparent bg-opacity-[.5]"
-                    type="text"
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                  ></input>
-                </p>
-                <p className="mt-1">Email: {userData.email}</p>
-                <p className="mt-1">
-                  Birthday:
-                  <input
-                    className="ring-slate-200 ring-[1px] rounded-md ml-1 pl-2 w-[150px] bg-transparent bg-opacity-[.5]"
-                    type="date"
-                    value={birthday}
-                    onChange={(e) => setBirthday(e.target.value)}
-                  ></input>
-                </p>
+            <div className="bg-slate-100 bg-opacity-[.15] mr-10 rounded-md w-[60vw]">
+              <p className="text-2xl font-bold text-center bg-cH1 bg-opacity-[1] rounded-tl-md rounded-tr-md">
+                Settings
+              </p>
+              <hr className="border-slate-400"></hr>
+              <div className="flex flexError">
+                <div className="ml-2 flex-basis-1/3 flex-col flexError items-center">
+                  <p>Timezone: </p>
+                  <select
+                    className="bg-black bg-opacity-[.55] rounded-md p-1"
+                    onChange={updateTimezone}
+                    value={timezone}
+                  >
+                    Timezones
+                    <option className="bg-black bg-opacity-[.85]" value="UTC">
+                      UTC - Coordinated Universal Time
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" value="EST">
+                      EST - Eastern Standard Time
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" alue="CST">
+                      CST - Central Standard Time
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" value="MST">
+                      MST - Mountain Standard Time
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" value="PST">
+                      PST - Pacific Standard Time
+                    </option>
+                  </select>
+                </div>
+                <div className="ml-2 flex-basis-1/3 flex-col flexError items-center">
+                  <p>Buzz Time: </p>
+                  <select
+                    className="bg-black bg-opacity-[.55] rounded-md p-1"
+                    onChange={updateBuzzTime}
+                    value={buzzTime}
+                  >
+                    Time of day
+                    <option className="bg-black bg-opacity-[.85]" value="5AM">
+                      5:00 AM
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" value="10AM">
+                      10:00 AM
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" alue="CST">
+                      Noon
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" value="MST">
+                      2:00 PM
+                    </option>
+                    <option className="bg-black bg-opacity-[.85]" value="PST">
+                      8:00 PM
+                    </option>
+                  </select>
+                </div>
               </div>
-              <button
-                className="bg-[#404F60] rounded-bl-md rounded-br-md mr-2 pl-4 pr-4 h-[40px] w-full hover:bg-opacity-[.8]"
-                onClick={saveAccountEdits}
-              >
-                Save
-              </button>
             </div>
           </div>
-          <div className="bg-slate-100 bg-opacity-[.15] mr-10 rounded-md">
+          <div className="self-center mt-20">
             <Countdown birthday={userData.birthday} />
           </div>
         </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="bg-[url('/static/images/App_Background.png')] w-[100vw] h-[100vh] bg-cover">
+        <PageHeader />
       </div>
     );
   }
